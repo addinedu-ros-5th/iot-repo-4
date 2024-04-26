b7d8993b9367d1c1c1c0ba9975e086ddf8c45526
goosebomb <as379907as@gmail.com>
2024-04-26 13:22:05 +0900

    FEAT : Code : GarbageTruck

--- SmartCity/CityEmbedded/GarbageTruck.ino
+++ SmartCity/CityEmbedded/GarbageTruck.ino
@@ -1,19 +1,57 @@
-#include <SoftwareSerial.h>
+#include <Servo.h>
 
-const int L_MOTOR_CW = 5;
-const int L_MOTOR_CCW = 6;
-const int R_MOTOR_CW = 9;
-const int R_MOTOR_CCW = 10;
+const int L_MOTOR_CW = 2;
+const int L_MOTOR_CCW = 4;
+const int R_MOTOR_CW = 7;
+const int R_MOTOR_CCW = 8;
 
 const int L_LINESENS = 12;
 const int R_LINESENS = 13;
 
-const int RX = 3;
-const int TX = 2;
+const int SERVO = 10;
 
 int speed = 150;
+char cmd;
 
-SoftwareSerial Bluetooth(RX, TX);
+Servo servo;
+
+typedef enum
+{
+  WAIT,
+  MOVING,
+  THROW,
+  STOP
+} STATUS;
+
+STATUS status;
+
+void setMotorAngle(int angle, int status)
+{
+  servo.attach(SERVO);
+  if (servo.attached())
+  {
+    if (status == 0)
+    {
+      servo.write(angle);
+      delay(100);
+      servo.detach();
+    }
+    else if (status == 1)
+    {
+      for (int i = angle; i >= 0; i--)
+      {
+        servo.write(i);
+        delay(2);
+      }
+      for (int i = 0; i <= angle; i++)
+      {
+        servo.write(i);
+        delay(2);
+      }
+      servo.detach();
+    }
+  }
+}
 
 void move(int leftCw, int leftCcw, int rightCw, int rightCcw)
 {
@@ -26,11 +64,16 @@ void move(int leftCw, int leftCcw, int rightCw, int rightCcw)
 void setup() 
 {
   Serial.begin(9600);
-  Bluetooth.begin(9600);
+  while (!Serial);
+
+  setMotorAngle(100, 0);
+
   pinMode(L_MOTOR_CW, OUTPUT);
   pinMode(L_MOTOR_CCW, OUTPUT);
   pinMode(R_MOTOR_CW, OUTPUT);
   pinMode(R_MOTOR_CCW, OUTPUT);
+
+  status = STATUS::WAIT;
 }
 
 void loop() 
@@ -38,23 +81,54 @@ void loop()
   int leftSensor = digitalRead(L_LINESENS);
   int rightSensor = digitalRead(R_LINESENS);
 
-  if (Bluetooth.available()) { Serial.write(Bluetooth.read()); }
-  if (Serial.available()) {Bluetooth.write(Serial.read()); }
+  if(Serial.available())
+  {
+    char cmd = Serial.read();
 
-  if (leftSensor == LOW && rightSensor == LOW)
-  {
-    move(speed, 0, speed, 0);
+    switch(cmd)
+    {
+      case '1':
+        status = STATUS::MOVING;
+        Serial.println("move to trash can");
+        break;
+      case '2':
+        status = STATUS::STOP;
+        Serial.println(status);
+        break;
+      case '3':
+        status = STATUS::THROW;
+        Serial.println(status);
+        break;
+      default:
+        break;
+    }
+    cmd = "";
   }
-  else if (leftSensor == HIGH && rightSensor == LOW)
+
+  if (status == STATUS::MOVING)
   {
-    move(0, speed, speed, 0);
+    if (leftSensor == LOW && rightSensor == LOW)
+    {
+      move(speed, 0, speed, 0);
+    }
+    else if (leftSensor == HIGH && rightSensor == LOW)
+    {
+      move(0, speed, speed, 0);
+    }
+    else if (leftSensor == LOW && rightSensor == HIGH)
+    {
+      move(speed, 0, 0, speed);
+    }
   }
-  else if (leftSensor == LOW && rightSensor == HIGH)
-  {
-    move(speed, 0, 0, speed);
-  }
-  else // leftSensor == HIGH && rightSensor == HIGH
+
+  if (status == STATUS::STOP)
   {
     move(0, 0, 0, 0);
+  }
+
+  if (status == STATUS::THROW)
+  {
+    status = STATUS::WAIT;
+    setMotorAngle(100, 1);
   }
 }
